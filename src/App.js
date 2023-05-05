808294ecab4238a1c3cf3728a0dc0855e5a07659
richard <36215881+hoganri@users.noreply.github.com>
2023-05-05 14:22:11 -0400

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from './App.css';
+import bitcoinMessage from 'bitcoinjs-message';
+
+window.Buffer = window.Buffer || require("buffer").Buffer;
 
 function App() {
+  const [isVerified, setIsVerified] = useState('');
+
+  const bitcoinMessageVerify = (e) => {
+    e.preventDefault();
+    try {
+      let result = bitcoinMessage.verify(e.target[1].value, e.target[0].value, e.target[2].value);
+      if (result) {
+        setIsVerified(true);
+      } else {
+        setIsVerified(false);
+      }
+    } catch(e) {
+      setIsVerified(e);
+    }
+  };
+  
+  // let x = bitcoinMessageVerify('test','1HvaxYmChk9nQUZx888a6hc7E8KnKP98ic','HCfgnEo8RNYTgcOXafAf11BQQ5LXSvlss0AbnezENbFJQzRl0dJRGOfYbOrOPkb2iPdj21Cty5G4paevOks/pJ4=');
+  // console.log(x);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Signature Verification</h1>
+      <form onSubmit={e => bitcoinMessageVerify(e)}>
+        <input placeholder="Address" />
+        <br />
+        <textarea placeholder="Message"></textarea>
+        <br />
+        <textarea placeholder="Signature"></textarea>
+        <br />
+        <button>Verify</button>
+      </form>
+      <div className="results">
+        {isVerified !== '' ? (
+          <>
+            {isVerified.toString()}
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 }
